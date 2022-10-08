3a7adfac5662485a61820d7ce6ca969c4af7be93
whitewizard2002 <70932466+whitewizard2002@users.noreply.github.com>
2022-10-08 10:41:56 +0530

    SqlFiles Addition

---
+++ dbo.LaundryRecord.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[LaundryRecord] (
+    [Id]               INT           IDENTITY (1, 1) NOT NULL,
+    [Name]             NVARCHAR (50) NOT NULL,
+    [NoOfItem]         INT           NOT NULL,
+    [Weight]           INT           NOT NULL,
+    [Price]            BIGINT        NOT NULL,
+    [Pending]          BIT           NOT NULL,
+    [Completed]        BIT           NOT NULL,
+    [Paid]             BIT           NOT NULL,
+    [Gender]           NVARCHAR (3)  NOT NULL,
+    [DateOfCompletion] DATE          NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ dbo.Users.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]       INT            IDENTITY (1, 1) NOT NULL,
+    [Name]     NVARCHAR (50)  NOT NULL,
+    [Gender]   NVARCHAR (50)  NOT NULL,
+    [Email]    NVARCHAR (100) NOT NULL,
+    [Password] NVARCHAR (50)  NOT NULL,
+    [PhoneNo]  NVARCHAR (10)  NOT NULL,
+    [Address]  NVARCHAR (150) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
